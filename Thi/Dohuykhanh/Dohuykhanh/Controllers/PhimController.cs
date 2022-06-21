3c6ff9fd733388e8b714f9e880cb0937853c5eff
dohuykhanh <88757246+dohuykhanh@users.noreply.github.com>
2022-06-21 09:55:09 +0700

    dohuykhanh
    emxinnop

---
+++ Thi/Dohuykhanh/Dohuykhanh/Controllers/PhimController.cs
@@ -0,0 +1,24 @@
+﻿using Dohuykhanh.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dohuykhanh.Controllers
+{
+    public class PhimController : Controller
+    {
+        // GET: Phim
+        Model1 data = new Model1();
+        public ActionResult Index()
+        {
+            
+            
+            
+                var all_phim = from s in data.Phims select s;
+                return View(all_phim);
+            
+        }
+    }
+}
---
+++ Thi/Dohuykhanh/Dohuykhanh/Models/Giohang.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Dohuykhanh.Models
+{
+    public class Giohang
+    {
+        Model1 data = new Model1();
+
+        [Display(Name = "Ma lich")]
+        public String malich { get; set; }
+        [Display(Name = "Ma phim")]
+        public int maphim { get; set; }
+        
+        [Display(Name = "Giá bán")]
+        public Double gia { get; set; }
+        [Display(Name = "Số lượng")]
+        public int iSoluong { get; set; }
+        [Display(Name = "Thành tiền")]
+        public Double dThanhtien
+        {
+            get { return iSoluong * gia; }
+        }
+        public Giohang(int maphim)
+        {
+            this.maphim = maphim;
+            LichChieu rubik = data.LichChieux.Single(n => n.maphim == maphim);
+            malich = rubik.malich;
+            
+            gia = double.Parse(rubik.gia.ToString());
+            iSoluong = 1;
+
+        }
+    }
+}
